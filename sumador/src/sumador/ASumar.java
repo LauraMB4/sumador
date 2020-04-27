62a76113c63a83910d7128980ded7946f741d468
user <user@DESKTOP-SUVLE5S>
2020-04-27 17:29:27 +0200

    Tercera prueba

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -2,15 +2,30 @@ package sumador;
 
 public class ASumar {
 
+	/** 
+	 * El metodo que recibe un String de tipo numerico y devuelve la suma de los caracteres/cifras
+	 * Si es negativo no devuelve nada
+	 * @param string cadena a convertir
+	 * @return la cadena = suma de sus digitos
+	 */
 	public String mostrar(String string) {
 		// TODO Auto-generated method stub
-		String total = string;
 		
 		if (string.length()==1)  
-			return (string + " = " + total);
+			return (string + " = " + string);
 		if (string.charAt(0)=='-')
 			return null;
-		return total;
+		return (string + " = " + total(string));
 	}
 
+	public int total(String valor_inicial) {
+        int suma = 0;
+
+        for (int i = 0; i < valor_inicial.length(); i++) {
+            // Cadena de un dígito.
+            String digito = valor_inicial.substring(i, i+1);
+            suma = suma + Integer.parseInt(digito);
+        }
+        return suma;
+	}
 }
--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -1,29 +1,40 @@
 package sumador;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNull;
 import org.junit.jupiter.api.Test;
 
 
 class TestASumar {
-	/**
-	 * Prueba para una cifra
-	 *
-	 */
-	 @Test
-	    public void testUnaCifra() {
-	        ASumar sumi = new ASumar();
-	        String resultado = sumi.mostrar("7");
-	        assertEquals("7 = 7",resultado );
-	  }
-
-	 /**
-	  * Prueba para cifra negativa
-	  *
-	  */	 
-	 @Test
-	    public void testNegativo() {
-	    	ASumar sumi= new ASumar();
-	    	String resultado = sumi.mostrar("-7");
-	    	assertNull(resultado);
-	    }
+		/**
+		 * Prueba para una cifra
+		 *
+		 */
+		@Test
+		public void testUnaCifra() {
+		ASumar sumi = new ASumar();
+		String resultado = sumi.mostrar("7");
+		assertEquals("7 = 7",resultado);
+		}
+		
+		/**
+		 * Prueba para cifra negativa
+		 *
+		 */	 
+		@Test
+		public void testNegativo() {
+		ASumar sumi= new ASumar();
+		String resultado = sumi.mostrar("-7");
+		assertNull(resultado);
+		}
+	 
+		/**
+		 * Prueba para cifra varios digitos
+		 */
+		@Test
+		public void testVariasCifra() {
+		ASumar sumi = new ASumar();
+		String resultado = sumi.mostrar("3433");
+		assertEquals("3433 = 13",resultado);
+		}	 
 }
