a8d3e317db0287d0acc82d5994c475ce74896cf9
user <user@DESKTOP-SUVLE5S>
2020-04-27 13:10:17 +0200

    Primera prueba

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -2,4 +2,11 @@ package sumador;
 
 public class ASumar {
 
+	public String mostrar(String string) {
+		// TODO Auto-generated method stub
+		String total = string;
+		
+		return (string + " = " + total);
+	}
+
 }
--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 
 class TestASumar {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	 @Test
+	    public void testUnaCifra() {
+	        ASumar sumi = new ASumar();
+	        String resultado = sumi.mostrar("7");
+	        assertEquals("7 = 7",resultado );
+	  }
 
 }
